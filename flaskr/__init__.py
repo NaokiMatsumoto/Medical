9cccfe78cb98560fef5c48602f55c62e35014620
test-nm <nmatsumoto9@gmail.com>
2020-11-28 17:03:18 +0900

    layout change

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 from flask_login import LoginManager
 
 login_manager = LoginManager() # Flask-Loginライブラリとアプリケーションをつなぐ
@@ -14,7 +14,7 @@ login_manager.login_message = 'ログインしてください'
 
 basedir = os.path.abspath(os.path.dirname(__name__))
 db = SQLAlchemy()
-migrate = Migrate()
+#migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -25,6 +25,6 @@ def create_app():
     from flaskr.views import bp
     app.register_blueprint(bp)
     db.init_app(app)
-    migrate.init_app(app, db)
+#    migrate.init_app(app, db)
     login_manager.init_app(app)
     return app
